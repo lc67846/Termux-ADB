2ec6f17a69d0e4fd05a3eca7020cc919cb11af83
#Wifi Got Minecraft <64691261+lc67846@users.noreply.github.com>
2021-03-10 18:06:54 -0500

    Update InstallTools.sh

--- InstallTools.sh
+++ InstallTools.sh
@@ -9,9 +9,9 @@ mkdir $PREFIX/tmp/adbtemp
 downpath="$PREFIX/tmp/adbtemp"
 wget https://github.com/MasterDevX/Termux-ADB/raw/master/bin/adb -P $downpath/ -q
 echo -e "\e[32m[*] \e[34mDownloading binaries..."
-wget https://github.com/MasterDevX/Termux-ADB/raw/master/bin/adb.bin -P $downpath/ -q
-wget https://github.com/MasterDevX/Termux-ADB/raw/master/bin/fastboot -P $downpath/ -q
-wget https://github.com/MasterDevX/Termux-ADB/raw/master/bin/fastboot-armeabi -P $downpath/ -q
+wget https://github.com/lc67846/Termux-ADB/raw/master/bin/adb.bin -P $downpath/ -q
+wget https://github.com/lc67846/Termux-ADB/raw/master/bin/fastboot -P $downpath/ -q
+wget https://github.com/lc67846/Termux-ADB/raw/master/bin/fastboot-armeabi -P $downpath/ -q
 echo -e "\e[32m[*] \e[34mCopying files to bin..."
 cp $downpath/* $PREFIX/bin
 echo -e "\e[32m[*] \e[34mSetting execution permissions..."
