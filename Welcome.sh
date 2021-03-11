3a7ea3f22ab31e3ffe0cd9905b63112a5efdbd30
#Wifi Got Minecraft <64691261+lc67846@users.noreply.github.com>
2021-03-11 16:33:31 -0500

    Update Welcome.sh

--- Welcome.sh
+++ Welcome.sh
@@ -1,3 +1,4 @@
 echo -e "\e[32mWelcome To Termux ADB Version 1.1\e[39m"
 echo Whats New
-apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://github.com/lc67846/Termux-ADB/raw/master/changelog.sh q && bash changelog.sh 
+echo added logo 
+echo -e "\e[32mChanged Some Installtools.sh Text\e[39m" 
