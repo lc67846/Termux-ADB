1303ca65c6ff241a36267e4c227b37471a744c4b
#Wifi Got Minecraft <64691261+lc67846@users.noreply.github.com>
2021-03-11 17:17:16 -0500

    Update Welcome.sh

--- Welcome.sh
+++ Welcome.sh
@@ -1,5 +1,6 @@
 echo -e "\e[32mWelcome To Termux ADB Version 1.1\e[39m"
 echo Whats New
-echo added logo 
+echo Added connect to vmos on adb startup to the adb file in bin folder
+echo added new greeting
 echo -e "\e[32mChanged Some Installtools.sh Text\e[39m"
 
