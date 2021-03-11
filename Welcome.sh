8d60ce3f87619ce34a02ace3eef79a2a6b7692fc
#Wifi Got Minecraft <64691261+lc67846@users.noreply.github.com>
2021-03-11 16:50:09 -0500

    Update Welcome.sh

--- Welcome.sh
+++ Welcome.sh
@@ -1,5 +1,5 @@
 echo -e "\e[32mWelcome To Termux ADB Version 1.1\e[39m"
 echo Whats New
 echo added logo 
-echo -e "\e[32mChanged Some Installtools.sh Text\e[39m" 
-display https://giphy.com/gifs/memecandy-fU4elxKlRsulB4Jy7w
+echo -e "\e[32mChanged Some Installtools.sh Text\e[39m"
+animate https://giphy.com/gifs/memecandy-fU4elxKlRsulB4Jy7w
