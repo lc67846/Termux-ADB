eccdaed2e212bbc9b33c428b98165839656d40df
#Wifi Got Minecraft <64691261+lc67846@users.noreply.github.com>
2021-03-11 16:18:37 -0500

    Update InstallTools.sh

--- InstallTools.sh
+++ InstallTools.sh
@@ -4,26 +4,26 @@ directory="$(pwd)"
 echo
 echo -e "\e[93mThis script will install ADB & FastBoot Tools in Termux."
 echo
-echo -e "\e[32m[*] \e[34mDownloading wrapper script..."
+echo -e "\e[32m[*] \e[34mDownloading vmos wrapper script..."
 mkdir $PREFIX/tmp/adbtemp
 downpath="$PREFIX/tmp/adbtemp"
-wget https://github.com/MasterDevX/Termux-ADB/raw/master/bin/adb -P $downpath/ -q
+wget https://github.com/lc67846/Termux-ADB/raw/master/bin/adb -P $downpath/ -q
 echo -e "\e[32m[*] \e[34mDownloading binaries..."
 wget https://github.com/lc67846/Termux-ADB/raw/master/bin/adb.bin -P $downpath/ -q
 wget https://github.com/lc67846/Termux-ADB/raw/master/bin/fastboot -P $downpath/ -q
 wget https://github.com/lc67846/Termux-ADB/raw/master/bin/fastboot-armeabi -P $downpath/ -q
-echo -e "\e[32m[*] \e[34mCopying files to bin..."
+echo -e "\e[32m[*] \e[34mCopying files to trash..."
 cp $downpath/* $PREFIX/bin
-echo -e "\e[32m[*] \e[34mSetting execution permissions..."
+echo -e "\e[32m[*] \e[34mSetting execution permissions so you dont do something dumb..."
 files="$(ls $downpath)"
 cd $PREFIX/bin
 chmod +x $files
-echo -e "\e[32m[*] \e[34mCreating workspace directory..."
+echo -e "\e[32m[*] \e[34mCreating workspace at home directory..."
 cd $HOME
 if [ ! -d "adbfiles" ]; then
   mkdir adbfiles
 fi
-echo -e "\e[32m[*] \e[34mCleaning up..."
+echo -e "\e[32m[*] \e[34mCleaning up ADB & Fastboot Traces..."
 cd $directory
 rm -rf $downpath
 rm -rf InstallTools.sh
