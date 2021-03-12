7b116a334f10b24afe315f74c7bb02dfa5169ccf
#Wifi Got Minecraft <64691261+lc67846@users.noreply.github.com>
2021-03-12 16:40:50 -0500

    Update Welcome.sh

--- Welcome.sh
+++ Welcome.sh
@@ -1,4 +1,4 @@
-echo -e "\e[32mWelcome To Termux ADB Version 1.1\e[39m"
+echo -e "\e[32mWelcome To Termux ADB Version 1.2\e[39m"
 echo Whats New
 echo -e "\e[39mMade Sure That The Welcome Deleted After Executed/e[32m"
 rm -rf Welcome.sh
