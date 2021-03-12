00e050bf845fe506c0d9181149e715efe152c876
#Wifi Got Minecraft <64691261+lc67846@users.noreply.github.com>
2021-03-12 16:34:46 -0500

    Update Welcome.sh

--- Welcome.sh
+++ Welcome.sh
@@ -3,4 +3,4 @@ echo Whats New
 echo Added connect to vmos on adb startup to the adb file in bin folder
 echo added new greeting
 echo -e "\e[32mChanged Some Installtools.sh Text\e[39m"
-
+rm -rf Welcome.sh
